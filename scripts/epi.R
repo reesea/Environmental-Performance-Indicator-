36ecdbcb0110620dbc774d9240497cea4b88ae4c
anthonyreese <dyq0132@gmail.com>
2018-01-17 11:07:06 +0900

    First commit.

---
+++ scripts/epi.R
@@ -0,0 +1,58 @@
+# Load packages
+require(dplyr)
+require(plotly)
+
+# Read in data
+setwd("E:/epi/scripts")
+epi <- readxl::read_xlsx("2016-epi.xlsx", sheet = 3)
+
+# View data
+View(epi)
+dim(epi)
+head(epi)
+tail(epi)
+xtabs(~Country, epi)
+str(epi)
+
+# Data Preprocessing
+names(epi)[names(epi) == 'Country'] <- 
+  'country'
+names(epi)[names(epi) == '2016 EPI Score'] <- 
+  'epi_score'
+names(epi)[names(epi) == 'Environmental Health'] <- 
+  'environmental_health'
+names(epi)[names(epi) == 'EH - Health Impacts'] <- 
+  'health_impacts'
+names(epi)[names(epi) == 'EH - Air Quality'] <- 
+  'air_quality'
+names(epi)[names(epi) == 'EV - Water Resources']<- 
+  'water_resources'
+names(epi)[names(epi) == 'Household Air Quality'] <- 
+  'household_air_quality'
+names(epi)[names(epi) == 'Access to Sanitation'] <- 
+  'access_to_sanitation'
+names(epi)[names(epi) == 'Access to Drinking Water'] <- 
+  'access_to_drinking_water'
+names(epi)[names(epi) == 'Wastewater Treatment'] <- 
+  'wastewater_treatment'
+
+epi <- epi[c(-1,-3,-5,-7,-10,-12,-13,-14,-15,-16,-17,-19,-20,-21,-22,
+             -23,-25,-27,-29,-30,-31,-32,-33,-34,-35,-36,-37,-38,
+             -39,-40)]
+
+# Structure of the data
+str(epi)
+
+# Summary of the data
+summary(epi)
+
+
+epi.plot = ggplot(epi, aes(x = air_quality, y = health_impacts)) +
+  geom_point() +
+  geom_smooth(method="lm")
+epi.plot
+
+epi.plot = ggplot(epi, aes(x = air_quality, y = environmental_health)) +
+  geom_point() +
+  geom_smooth(method="lm")
+epi.plot
